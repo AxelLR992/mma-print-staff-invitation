f0415081b764c826ffeee2038240a5a913a7bf4f
Axel León <yka992@hotmail.com>
2021-12-24 13:12:28 -0600

    Se guarda un archivo CSV con la información de las personas que se hayan registrado

--- services/printInvitation.php
+++ services/printInvitation.php
@@ -11,6 +11,18 @@ $email = htmlspecialchars($data->email);
 $logoMM = base64_encode(file_get_contents("../assets/images/manhattanmannor-logo.png"));
 $logoRosie = base64_encode(file_get_contents("../assets/images/rosie-logo.png"));
 
+# Save user information to CSV
+$content = $firstName . "," . $lastName . "," . $email . "\r";
+$file = "../registeredUsers.csv";
+if (is_file($file)) {
+    file_put_contents($file, $content, FILE_APPEND);
+} else {
+    $csv = fopen($file, "w") or die("Unable to create CSV!");
+    fwrite($csv, $content);
+    fclose($csv);
+}
+
+# Create PDF from HTML Template
 $htmlTemplate = fopen("../assets/templates/guestInvitationTemplate.html", "r") or die("Unable to open template!");
 $htmlContent = fread($htmlTemplate, filesize("../assets/templates/guestInvitationTemplate.html"));
 $htmlContent = str_replace("{{guestFirstName}}", $firstName, $htmlContent);
